d568637836204c9fd758f2814156c696cdbbb83e
Ruan <ruan_kaylo@yahoo.com.br>
2017-08-03 10:58:52 -0300

    Adicionei a activity_upgrades

---
+++ app/src/main/java/com/example/ruan/keydclicker/UpgradesActivity.java
@@ -0,0 +1,17 @@
+package com.example.ruan.keydclicker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ruan on 03/08/17.
+ */
+
+public class UpgradesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upgrades);
+    }
+}
